81799327dc3b7b578929d12c95805f3db42577e8
atakanteko <atakantekoglu@gmail.com>
2022-04-27 10:41:25 +0300

    Update TheMoveToEarn.js

--- components/TheMoveToEarn.js
+++ components/TheMoveToEarn.js
@@ -6,9 +6,10 @@ import MoveToEarnText from './TheMoveToEarn/MoveToEarnText';
 export const TheMoveToEarn = () => {
     return (
         <div className={`sm:bg-top sm:bg-resp-home-cover sm:px-6
-                         xl:bg-resp-home-cover
-                         md:bg-right md:bg-home md:px-6
-                         px-2 h-screen bg-cover bg-home-cover relative overflow-hidden`}>
+                         md:bg-right md:bg-resp-home-cover md:px-6
+                         lg:bg-right lg:bg-resp-home-cover lg:px-6
+                         xl:bg-home-cover
+                         px-2 h-screen bg-cover bg-move-to-earn-cover relative overflow-hidden`}>
             <div className="sm:mx-1 sm:flex sm:flex-col
                             md:mx-1 md:flex md:flex-col
                             mx-192">
