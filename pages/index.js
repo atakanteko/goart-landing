e18f87cf312d92d390f1a4018be1df7e366e5b2f
atakanteko <atakantekoglu@gmail.com>
2022-04-29 00:34:32 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import { TheMoveToEarn } from "../components/TheMoveToEarn";
 import { TheSectionBgCover } from "../components/TheSectionBgCover";
 import { TheWalkingGirl} from "../components/TheWalkingGirl";
-import { TheMeetTeam } from "../components/TheMeetTeam";
+// import { TheMeetTeam } from "../components/TheMeetTeam";
 import { ThePurpleBicycle } from "../components/ThePurpleBicycle";
 import { TheAboutUs } from "../components/TheAboutUs";
 import { TheRoadMap } from "../components/TheRoadMap";
